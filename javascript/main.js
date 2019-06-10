2d10a482ed095fca3576c5be1ab6d3c007160e70
AlexisDuval <alexis.duval.8@gmail.com>
2019-06-10 15:49:47 -0400

    ajout d'une rotation onclick

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,12 @@
 
 window.onload = () => {
 
-	alert("test");
+	let degres = 7;
 
+	let ligne1 = document.getElementById("ligne-1");
 
+	ligne1.onclick = () => {
+		degres += 7;
+		ligne1.style.transform = "rotate(" + degres + "deg)";
+	}
 }
