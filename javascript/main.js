3ebefd88b927e7e73dc46346f23e096062aa58eb
AlexisDuval <alexis.duval.8@gmail.com>
2019-06-10 16:29:37 -0400

    rotation relativement stable et propre

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,28 @@
 
+const delai = 42;
+const valeurPrChanger = 75;
+let acceleration = 1;
+let vitesse = 21;
+
 window.onload = () => {
 
-	let degres = 7;
-
+	let degres = 0;
 	let ligne1 = document.getElementById("ligne-1");
 
 	ligne1.onclick = () => {
-		degres += 7;
-		ligne1.style.transform = "rotate(" + degres + "deg)";
+
+		setInterval(function() {
+
+			vitesse += acceleration;
+			degres += vitesse;
+			degres %= 360;
+			ligne1.style.transform = "rotate(" + degres + "deg)";
+
+			if (vitesse > valeurPrChanger) {acceleration = -1;}
+			else if (vitesse < -valeurPrChanger) {acceleration = 1;}
+
+			console.log(vitesse);
+
+		}, delai);
 	}
 }
