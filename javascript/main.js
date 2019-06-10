d50143052c12d26a514545e714d58d5925d4f6bd
AlexisDuval <alexis.duval.8@gmail.com>
2019-06-10 16:53:11 -0400

    modification de la structure du code

--- javascript/main.js
+++ javascript/main.js
@@ -1,28 +1,32 @@
 
-const delai = 42;
-const valeurPrChanger = 75;
-let acceleration = 1;
-let vitesse = 21;
+let ligne1 = null;
 
 window.onload = () => {
+	ligne1 = document.getElementById("ligne-1");
+	animerCarre();
+}
 
-	let degres = 0;
-	let ligne1 = document.getElementById("ligne-1");
+const animerCarre = () => {
 
 	ligne1.onclick = () => {
 
-		setInterval(function() {
+		const delai = 42;
+		const valeurPrChanger = 75;
+		let acceleration = 1;
+		let vitesse = 21;
+		let degres = 0;
+
+		setInterval(function () {
 
 			vitesse += acceleration;
 			degres += vitesse;
 			degres %= 360;
 			ligne1.style.transform = "rotate(" + degres + "deg)";
 
-			if (vitesse > valeurPrChanger) {acceleration = -1;}
-			else if (vitesse < -valeurPrChanger) {acceleration = 1;}
-
-			console.log(vitesse);
+			if (vitesse > valeurPrChanger) { acceleration = -1; }
+			else if (vitesse < -valeurPrChanger) { acceleration = 1; }
 
 		}, delai);
+
 	}
 }
